ae67a65b39cdce2b061416d4213030d5c2ac0dd5
Gnelezie <zie.nanien@gmail.com>
2023-02-27 13:50:03 +0000

    Mise à jour

--- function/domaine.php
+++ function/domaine.php
@@ -1,4 +1,4 @@
 <?php
-$domaine = 'http://localhost:8080/www/ccgim';
-$cdn_domaine = 'http://localhost:8080/www/ccgim/cdn';
+$domaine = 'http://localhost:8080/www/app-ccgim';
+$cdn_domaine = 'http://localhost:8080/www/app-ccgim/cdn';
 $cookies_domaine = '.localhost:8080';
